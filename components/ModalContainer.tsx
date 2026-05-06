3461d9d73d5565857932dc0f23dded280bd10492
delete-44 <delete44+tech@protonmail.com>
2026-05-06 15:11:06 +0100

    Improvements to modal styling + added backdrop to let user tap outside to close it

--- components/ModalContainer.tsx
+++ components/ModalContainer.tsx
@@ -3,6 +3,7 @@ import { cross } from "@/assets/icons/cross";
 import {
   BACKGROUND_COLOR,
   BACKGROUND_COLOR_HIGHLIGHT,
+  CONTENT_BACKDROP,
   CONTENT_COLOR,
   DECORATION_COLOR,
   SPACING_LG,
@@ -25,38 +26,64 @@ export default function ModalContainer({
   onClose,
 }: Props) {
   return (
-    <Modal animationType="fade" transparent={true} visible={isVisible}>
-      <View style={styles.modalContent}>
-        <View style={styles.titleContainer}>
-          <Text style={globalStyles.textMd}>{title}</Text>
-          <Pressable
-            onPress={onClose}
-            role="button"
-            accessibilityLabel="Close Modal"
-            style={globalStyles.buttonPlain}
-          >
-            <SVG icon={cross} width={22} height={22} color={CONTENT_COLOR} />
-          </Pressable>
-        </View>
+    <Modal
+      animationType="fade"
+      transparent
+      visible={isVisible}
+      onRequestClose={onClose}
+      navigationBarTranslucent
+      statusBarTranslucent
+    >
+      {/* Backdrop to allow user to click out of the modal to close it */}
+      <Pressable
+        style={[StyleSheet.absoluteFill, { backgroundColor: CONTENT_BACKDROP }]}
+        onPress={() => onClose()}
+        accessibilityLabel="Close Modal"
+        role="button"
+      />
 
-        <View style={styles.modalBody}>{children}</View>
+      <View style={styles.modalWrapper}>
+        <View
+          style={styles.modalContent}
+          pointerEvents="box-none"
+          accessibilityViewIsModal
+        >
+          <View style={styles.titleContainer}>
+            <Text style={globalStyles.textMd}>{title}</Text>
+            <Pressable
+              onPress={onClose}
+              role="button"
+              accessibilityLabel="Close Modal"
+              style={globalStyles.buttonPlain}
+            >
+              <SVG icon={cross} width={22} height={22} color={CONTENT_COLOR} />
+            </Pressable>
+          </View>
+
+          <View style={styles.modalBody}>{children}</View>
+        </View>
       </View>
     </Modal>
   );
 }
 
 const styles = StyleSheet.create({
+  modalWrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   modalContent: {
     maxHeight: "50%",
     backgroundColor: BACKGROUND_COLOR,
     borderRadius: SPACING_SM,
     borderColor: DECORATION_COLOR,
     borderWidth: 5,
-    top: 180,
     marginInline: SPACING_LG * 2,
+    overflow: "hidden",
   },
   titleContainer: {
-    padding: SPACING_SM,
+    paddingLeft: SPACING_SM, // Only pad at start - button has padding already
     backgroundColor: BACKGROUND_COLOR_HIGHLIGHT,
     flexDirection: "row",
     alignItems: "center",
@@ -66,5 +93,6 @@ const styles = StyleSheet.create({
   },
   modalBody: {
     padding: SPACING_SM,
+    flex: 1,
   },
 });
