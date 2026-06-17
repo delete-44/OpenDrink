287e4402c972039a1a6bf9ff65fc2986d9fe3e50
delete-44 <delete44+tech@protonmail.com>
2026-06-17 10:59:08 +0100

    Next pass at icons & deck logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,24 +1,12 @@
-import { CONTENT_COLOR, SPACING_MD } from "@/src/constants/style-constants";
 import { Image } from "expo-image";
-import { StyleSheet, View } from "react-native";
 
 export default function Logo() {
   return (
-    <View style={styles.logoBackground}>
-      <Image
-        source={require("../../assets/images/decorative/deck.png")}
-        alt=""
-        role="img"
-        style={{ width: 96, height: 96 }}
-      />
-    </View>
+    <Image
+      source={require("../../assets/images/decorative/deck.png")}
+      alt=""
+      role="img"
+      style={{ width: 96, height: 96 }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  logoBackground: {
-    backgroundColor: CONTENT_COLOR,
-    borderRadius: 99,
-    padding: SPACING_MD,
-  },
-});
