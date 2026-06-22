f2f73bd10c6a676fd674ad4b8c585cce4b5bc896
delete-44 <delete44+tech@protonmail.com>
2026-06-22 15:36:59 +0100

    Added statusMEssage component that can be success/error

---
+++ src/components/StatusMessage.tsx
@@ -0,0 +1,56 @@
+import globalStyles from "@/assets/global-styles";
+import { check } from "@/assets/icons/check";
+import { circleAlert } from "@/assets/icons/circleAlert";
+import { Text } from "react-native";
+import { WARNING_COLOR } from "../constants/style-constants";
+import { StatusMessageProps } from "../types";
+import SVG from "./SVG";
+
+export default function StatusMessage({
+  type,
+  message,
+  describes,
+}: StatusMessageProps) {
+  if (!message) {
+    return null;
+  }
+
+  switch (type) {
+    case "success":
+      return (
+        <>
+          <SVG icon={check} color={WARNING_COLOR} width={18} height={18} />
+
+          <Text
+            style={globalStyles.textSuccess}
+            role="alert"
+            accessibilityLiveRegion="polite"
+            nativeID={`${describes}-status`}
+          >
+            {message}
+          </Text>
+        </>
+      );
+
+    default:
+      return (
+        <>
+          <SVG
+            icon={circleAlert}
+            color={WARNING_COLOR}
+            width={18}
+            height={18}
+          />
+
+          <Text
+            style={globalStyles.textWarning}
+            role="alert"
+            accessibilityLiveRegion="polite"
+            nativeID={`${describes}-status`}
+          >
+            {message}
+          </Text>
+        </>
+      );
+  }
+}
